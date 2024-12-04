7a74765f78dc1b766c1c5ee720da24c444163b02
vignesh <vignesh@localhost.localdomain>
2024-12-04 15:57:02 +0530

    License Update

--- core.py
+++ core.py
@@ -1,6 +1,12 @@
 import arklib
 from parser import yamlParser
 
+def dyno_vars():
+    #create variables to use in between the test steps
+    
+    pass
+
+
 def main():
     getDetails = yamlParser()
     for test in getDetails['testStep']:
